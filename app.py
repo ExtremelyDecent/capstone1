eb127ebb495aefefb18b76b607f7f9ac7e754446
Henry Frost <henry.90.frost@gmail.com>
2022-10-05 10:21:53 -0400

    starting code with base layout

---
+++ app.py
@@ -0,0 +1,49 @@
+from secrets import API_SECRET_KEY
+from flask import Flask, render_template,request, redirect, flash
+from forms import GetDataForm
+from models import db, connect_db, Champion, Tier, Region, Challenge, Map
+import requests
+
+key = API_SECRET_KEY
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = ('postgresql:///riotchallenges')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+API_BASE_URL = "americas.api.riotgames.com"
+champion_list = []
+challenge_list = []
+
+connect_db(app)
+
+@app.route('/')
+def show_homepage():
+    return render_template("index.html")
+    form = GetDataForm()
+
+@app.route('/champions')
+def show_champions():
+
+    return render_template("champions.html", champions = champion_list)
+
+@app.route('/champions/get')
+def get_champions():
+    res = requests.get("http://ddragon.leagueoflegends.com/cdn/12.18.1/data/en_US/champion", 
+        params={"term": "champions"})
+    for champion in res.data:
+        champion_list.push(champion)
+        add_champion(champion.name, f"/static/img/tiles/{champion.id}_0.jpg")
+    
+    return redirect('/champions')
+
+@app.route('/challenges')
+def show_challenges():
+    
+    return render_template("challenges.html", challenges = challenge_list)
+
+@app.route('/challenges/get')
+def get_challenges():
+    res = requests.get(f"{API_BASE_URL}/lol/challenges/v1/challenges/config")
+    challenge = res.data
+    add_challenge()
+    
+    return redirect('/challenges')
---
+++ models.py
@@ -0,0 +1,159 @@
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+
+class Champion(db.Model):
+    """All champions currently available for play"""
+    __tablename__ = "champions"
+
+    champion_id = db.Column(
+        db.Long,
+        primary_key = True
+    )
+    champion_name = db.Column(
+        db.Text,
+        nullable = False,
+        unique = True  
+    )
+    region_id = db.Column(
+        db.Long,
+        db.ForeignKey('regions.id', ondelete = 'cascade')
+    )
+    champion_image_url = db.Column(
+        db.Text,
+        nullable = False,
+    )
+    champion_role_id = db.Column(
+        db.Long,
+        db.ForeignKey('role.id', ondelete= 'cascade')
+    )
+    @classmethod
+    def add_champion(cls, champion_name, champion_image_url):
+        champion = Champion(
+            champion_name = champion_name,
+            champion_image_url = champion_image_url
+        )
+        db.session.add(champion)
+
+
+
+
+class Challenge(db.Model):
+    """Challenges curretly available in game"""
+    __tablename__ = "challenges"
+
+    challenge_id = db.Column(
+        db.Long,
+        primary_key = True
+    )
+    challenge_name = db.Column(
+        db.Text,
+        nullable = False,
+        unique = True  
+    )
+    challenge_region_id = db.Column(
+        db.Long,
+        db.ForeignKey('regions.id', ondelete = 'cascade')
+    )
+    challenge_image_url = db.Column(
+        db.Text,
+        nullable = False,
+    )
+    challenge_role_id = db.Column(
+        db.Long,
+        db.ForeignKey('role.id', ondelete= 'cascade')
+    )
+    chanllenge_map_id = db.Column(
+        db.Long.
+        db.ForeignKey('map.id', ondelete = 'cascade')
+    )
+    challenge_tier_id = db.Column(
+        db.Long
+        db.ForeignKey('tier.id', ondelete = 'cascade')
+    )
+
+    @classmethod
+    def add_challenge(cls, challenge_name, challenge_image_url):
+        challenge = Challenge(
+            challenge_name = challenge_name,
+            challenge_image_url = challenge_image_url
+        )
+        db.session.add(challenge)
+
+
+
+class Region(db.Model):
+    """Champion regions"""
+    __tablename__ = "regions"
+
+    region_id = db.Column(
+        db.Long,
+        primary_key = True
+    )
+    region_name = db.Column(
+        db.String(20),
+        nullable=False,
+        unique = True
+    )
+    region_image_url = db.Column(
+        db.Text,
+        nullable = False,
+    )
+
+class Map(db.Model):
+    """Maps"""
+    __tablename__ = "maps"
+
+    map_id = db.Column(
+        db.Long,
+        primary_key = True
+    )
+    map_name = db.Column(
+        db.String(20),
+        nullable=False,
+        unique = True
+    )
+
+class Tier(db.Model):
+    """Challenge tiers"""
+    __tablename__ = "tier"
+
+    tier_id = db.Column(
+        db.Long,
+        primary_key = True
+    )
+    tier_name = db.Column(
+        db.String(20),
+        nullable=False,
+        unique = True
+    )
+    tier_image_url = db.Column(
+        db.Text,
+        nullable = False,
+    )
+    tier_xp_max = db.Column(
+        db.Long,
+        nullable = false
+    )
+class Role(db.Model):
+    """Challenge tiers"""
+    __tablename__ = "roles"
+
+    role_id = db.Column(
+        db.Long,
+        primary_key = True
+    )
+    role_name = db.Column(
+        db.String(20),
+        nullable=False,
+        unique = True
+    )
+    role_image_url = db.Column(
+        db.Text,
+        nullable = False,
+    )
